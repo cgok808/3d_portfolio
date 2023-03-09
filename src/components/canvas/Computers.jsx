256206f60bb6b91ae6d237cb5ab00193024050a6
cgok808 <chaylin.gokan@gmail.com>
2023-03-08 16:07:01 -0800

    implemented 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; 
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }} //x, y, z axis. field of view
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* loader while model is loading */}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls //camera controls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2} //max angle you can move the camera up
+          minPolarAngle={Math.PI / 2} //max angle you can move the camera down
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
